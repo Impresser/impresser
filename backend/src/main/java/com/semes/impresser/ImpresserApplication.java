5c4fe1dcab44962864e6e5e6da1f417196ce0363
김환수 <skes01149@naver.com>
2025-10-23 17:52:28 +0900

    Initial commit

---
+++ backend/src/main/java/com/semes/impresser/ImpresserApplication.java
@@ -0,0 +1,13 @@
+package com.semes.impresser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImpresserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImpresserApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/semes/impresser/ImpresserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semes.impresser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpresserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
