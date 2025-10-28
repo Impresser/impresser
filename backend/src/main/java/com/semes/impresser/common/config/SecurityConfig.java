12ecc5d7aeb619e5285ca456cdd03194a2435ce0
김환수 <skes01149@naver.com>
2025-10-28 17:01:17 +0900

    [BE] chore: CI/CD 파이프라인 구축
    * [FE] chore: 불필요한 파일 제거
    
    - frontend 폴더 내 README.md 제거
    - frontend 폴더 내 .gitignore 제거
    
    * [INFRA] chore: CI/CD 관련 설정 파일 추가
    
    - Jenkinsfile 추가
    - nginx 설정 파일 추가
    
    * [IMAGE] chore: Dockerfile 추가
    
    * [FE] chore: Dockerfile 추가
    
    * [BE] chore: Dockerfile 추가
    
    - application.yml에 profile 설정 추가
    
    * [BE] feat: Health Check를 위한 Actuator 추가
    
    - Actuator 의존성 추가
    - SecurityConfig에 Health Check 경로 추가

--- backend/src/main/java/com/semes/impresser/common/config/SecurityConfig.java
+++ backend/src/main/java/com/semes/impresser/common/config/SecurityConfig.java
@@ -44,7 +44,8 @@ public class SecurityConfig {
                 .requestMatchers(
                     "/v3/api-docs/**",
                     "/swagger-ui/**",
-                    "/swagger-ui.html"
+                    "/swagger-ui.html",
+                    "/actuator/health"
                 ).permitAll()
                 .requestMatchers(HttpMethod.POST, "/area").hasRole("ADMIN")
                 .anyRequest().authenticated()
