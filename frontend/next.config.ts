b63e90b5fcf2727f84831c1ff07289d47705cf4c
김환수 <skes01149@naver.com>
2025-11-03 22:45:34 +0900

    [FE] feat: next.config.ts에 basePath 추가
    * [FE] feat: next.config.ts에 basePath 추가

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath: process.env.BASE_PATH || '',
+  assetPrefix: process.env.ASSET_PREFIX || '',
+  images: { path: `${process.env.BASE_PATH || ''}/_next/image` },
 };
 
 export default nextConfig;
