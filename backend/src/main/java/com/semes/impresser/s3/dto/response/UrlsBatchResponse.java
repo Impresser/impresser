0290a5a18cd7c1bd966f34bc77fb07216a7056e5
이희산 <heesan6615@naver.com>
2025-11-07 10:54:32 +0900

    [BE] refactor: 이미지 업로드 요청 반환값 수정
    * [BE] refactor: TIFF 이미지 contentType 수정
    
    * [BE] fix: image URL 발급 수정

--- backend/src/main/java/com/semes/impresser/s3/dto/response/UrlsBatchResponse.java
+++ backend/src/main/java/com/semes/impresser/s3/dto/response/UrlsBatchResponse.java
@@ -3,7 +3,7 @@ package com.semes.impresser.s3.dto.response;
 import java.util.List;
 
 public record UrlsBatchResponse(
-    List<UrlsBatchItem> items
+    List<UrlsBatchItemResponse> items
 ) {
 
 }
