c66e3e74f808feb3edcce9b9e3b05da18dc2ac3e
윤혜진 <yhjyhw1004@naver.com>
2025-11-17 22:06:10 +0900

    [FE] feat: 대시보드 가로 스크롤 제거
    * [FE] feat: 대시보드 가로 스크롤 제거
    
    * [FE] feat: 대시보드 가로 스크롤 제거

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -56,7 +56,7 @@ export default function DashboardPage() {
         <Sidebar />
 
         {/* Main Content */}
-        <div className="flex-1 flex flex-col">
+        <div className="flex-1 min-w-0 flex flex-col">
           {/* Navigation Bar */}
           <Navbar />
 
